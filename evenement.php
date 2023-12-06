0b8de2ed8a4e3c7c8c984a9e6bc2e4fc5685d8d7
Aymeri32 <aymeri.petermann@lycee-pardailhan.fr>
2023-12-06 14:29:58 +0100

    ddd

---
+++ evenement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GarageRoy - Produits</title>
+        <link rel="stylesheet" href="style.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    </head>
+    <header>
+        <center><a href="index.html"><img src="img/Logo.png"></a></center>
+    </header>
+
+    <body>
+        <span class="produit">Voici nos Evenement:</span>
+        <a href="sponsors.php" class="bouton-sponsors">Sponsors</a>
+        
+      
+        </ul>
+    </body>
+</html>
