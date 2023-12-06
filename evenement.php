744fe27746684e7686ac5637a69ee3d013eed902
Aymeri32 <aymeri.petermann@lycee-pardailhan.fr>
2023-12-06 16:02:25 +0100

    evenement

--- evenement.php
+++ evenement.php
@@ -6,18 +6,11 @@ $pwd = "btssio32";
 $database = "sponsoroy";
 $connexion = mysqli_connect($hostname, $user, $pwd, $database);
 
-
 if (!$connexion) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-
-$query = "SELECT nom_evenement FROM evenement";
-$result = $connexion-> query($query);
-while ($row = $result->fetch_row()) {
-    printf($row[0]);
+    die("Connection failed: " . mysqli_connect_error());
 }
   
-$connexion->close();
+
 
 
 ?>
@@ -39,22 +32,21 @@ $connexion->close();
         <a href="sponsors.php" class="bouton-sponsors">Sponsors</a>
         
 
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>Nom de l'événement</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row['nom_evenement'] . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+        <?php
+            $query = "SELECT nom_evenement FROM evenement";
+            $result = $connexion->query($query);
+
+            echo "<table border='1'>";
+            echo "<tr><th>Evenement</th></tr>";
+
+            while ($row = $result->fetch_row()) {
+                echo "<tr><td>" . $row[0] . "</td></tr>";
+            }
+
+            echo "</table>";
+
+            $connexion->close();
+        ?>
 
       
         </ul>
