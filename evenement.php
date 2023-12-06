92e96f7a7dc178fd185645fe32748ce0cf4a04f0
Aymeri32 <aymeri.petermann@lycee-pardailhan.fr>
2023-12-06 15:57:24 +0100

    evenement

--- evenement.php
+++ evenement.php
@@ -1,3 +1,27 @@
+<?php
+
+$hostname = "192.168.1.80";
+$user = "admin";
+$pwd = "btssio32";
+$database = "sponsoroy";
+$connexion = mysqli_connect($hostname, $user, $pwd, $database);
+
+
+if (!$connexion) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT nom_evenement FROM evenement";
+$result = $connexion-> query($query);
+while ($row = $result->fetch_row()) {
+    printf($row[0]);
+}
+  
+$connexion->close();
+
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -14,6 +38,24 @@
         <span class="produit">Voici nos Evenement:</span>
         <a href="sponsors.php" class="bouton-sponsors">Sponsors</a>
         
+
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Nom de l'événement</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['nom_evenement'] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+
       
         </ul>
     </body>
